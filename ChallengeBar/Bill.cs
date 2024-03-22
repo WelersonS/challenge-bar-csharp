298ab30a8f26ea3031f8283f28b10ef50e4a2c60
welersons <welerson58@gmail.com>
2024-03-21 21:46:24 -0300

    Challenge solved

---
+++ ChallengeBar/Bill.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace ChallengeBar
+{
+    class Bill
+    {
+        public static double BeerValue = 5.00;
+        public static double BarbecueValue = 7.00;
+        public static double SoftDrinkValue = 3.00;
+
+        public char gender;
+        public int beer;
+        public int barbecue;
+        public int softDrink;
+
+        public double Cover()
+        {
+            return this.Feeding() > 30.00 ? 0.0 : 4.00;
+        }
+        public double Feeding()
+        {
+            return (this.beer * BeerValue) + (this.barbecue * BarbecueValue ) + (this.softDrink * SoftDrinkValue);
+        }
+        public double Ticket()
+        {
+            if (Char.ToUpper(this.gender) == 'M')
+                return 10.00;
+            else
+                return 8.00;
+        }
+        public double Total()
+        {
+            double total = this.Ticket() + this.Feeding() + this.Cover();
+            return total;
+        }
+    }
+}
---
+++ ChallengeBar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace ChallengeBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bill bill = new Bill();
+
+            Console.Write("Sexo: ");
+            bill.gender = char.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de cervejas: ");
+            bill.beer= int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de refrigerantes: ");
+            bill.softDrink = int.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade de espetinhos: ");
+            bill.barbecue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("RELATÓRIO:");
+            Console.WriteLine($"Consumo = R$ {bill.Feeding().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            if (bill.Cover() > 0.0)
+            {
+                Console.WriteLine($"Couvert = R$ {bill.Cover().ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                Console.WriteLine("Isento de Couvert");
+            }
+            
+            Console.WriteLine($"Ingresso = R$ {bill.Ticket().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine();
+            Console.WriteLine($"Valor a pagar = R$ {bill.Total().ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
